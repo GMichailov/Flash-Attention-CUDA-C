14e715ce584c843948d27151c5aa840f0020685c
George Michailov <george.michailov.work@outlook.com>
2025-09-01 20:33:45 -0700

    Continued writing flash attention v1 without double buffering where each warp is responsible for a singular row.

--- helpers.hpp
+++ helpers.hpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 #include <cmath>
+#include <cassert>
 
 inline int calculateSizeBlockQ(cudaDeviceProp& prop, int d_head) {
     // Dynamic Calculation Formula: <= Registers in CTA / (d_head * sizeof(scalar_t)) and Registers in CTA = Registers Total / threads per CTA per SM
@@ -13,7 +14,8 @@ inline int calculateSizeBlockQ(cudaDeviceProp& prop, int d_head) {
     int brMax = threadArrRegisters / (d_head * scalarSize);
     constexpr int maxSize = 128*64;
     int experimentalLimit = maxSize / d_head;
-    return std::min(experimentalLimit, brMax); 
+    //return std::min(experimentalLimit, brMax); 
+    return 64;
 }
 
 inline int calculateSizeBlockKV(cudaDeviceProp& prop, int d_head, int device) {
@@ -23,5 +25,12 @@ inline int calculateSizeBlockKV(cudaDeviceProp& prop, int d_head, int device) {
     int BcMax = l2Cache / (2 * d_head * sizeof(float));
     constexpr int maxSize = 128*64;
     int experimentalLimit = maxSize / d_head;
-    return std::min(experimentalLimit, BcMax);
+    //return std::min(experimentalLimit, BcMax);
+    return 64;
+}
+
+
+inline int getNumCta(int q_dim, int q_block_size) {
+    assert(q_dim % q_block_size == 0 && "Block size of Q rows must cleanly split Q");
+    return q_dim / q_block_size;
 }
