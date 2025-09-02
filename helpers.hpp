9d555df8df8ef09ce025667ffceffd40c16d9a72
George Michailov <george.michailov.work@outlook.com>
2025-09-01 18:02:56 -0700

    Helpers for calculating the block sizes in GPU memory.

---
+++ helpers.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <cmath>
+
+inline int calculateSizeBlockQ(cudaDeviceProp& prop, int d_head) {
+    // Dynamic Calculation Formula: <= Registers in CTA / (d_head * sizeof(scalar_t)) and Registers in CTA = Registers Total / threads per CTA per SM
+    int warpsPerCta = 4;
+    int totalRegisters = prop.regsPerMultiprocessor;
+    int threadArrRegisters = totalRegisters / (warpsPerCta * prop.warpSize);
+    int scalarSize = sizeof(float); // Hardcode as float for now, change later.
+    int brMax = threadArrRegisters / (d_head * scalarSize);
+    constexpr int maxSize = 128*64;
+    int experimentalLimit = maxSize / d_head;
+    return std::min(experimentalLimit, brMax); 
+}
+
+inline int calculateSizeBlockKV(cudaDeviceProp& prop, int d_head, int device) {
+    // Dynamic Calculation Formula: <= Size L2 Cache / (2 * d_head * sizeof(scalar_t)) [x2 is for double buffering]
+    int l2Cache = 0;
+    cudaDeviceGetAttribute(&l2Cache, cudaDevAttrL2CacheSize, device);
+    int BcMax = l2Cache / (2 * d_head * sizeof(float));
+    constexpr int maxSize = 128*64;
+    int experimentalLimit = maxSize / d_head;
+    return std::min(experimentalLimit, BcMax);
+}
