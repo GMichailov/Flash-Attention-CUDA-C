cbc348859ae41321c4d428d5d9abfb46eb5e08f4
George Michailov <george.michailov.work@outlook.com>
2025-10-12 17:44:33 -0700

    Compilation check.

---
+++ tests/main.cu
@@ -0,0 +1,9 @@
+#include "FlashAttention.cuh"
+
+int main() {
+    twoLoaderMhaFlashAttentionKernel<64, 16, 16><<<1, 256>>>(
+        nullptr, nullptr, nullptr, nullptr,
+        1, 1, 1, 1.0f, false
+    );
+    return 0;
+}
